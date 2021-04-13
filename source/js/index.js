49dff57eb119754952a797eaa2c2106d9e4bdebd
a-a-a-aa <78693043+a-a-a-aa@users.noreply.github.com>
2021-04-13 16:38:15 -0700

    Add files via upload

---
+++ source/js/index.js
@@ -0,0 +1,10 @@
+
+
+let display_letter = () => {
+    let root = document.documentElement;
+    let base = document.getElementById('base')
+
+    root.style.setProperty('--page-height', '200px' );
+    document.getElementById('hny').style.display = "block";
+    base.classList.remove('increment')
+}
